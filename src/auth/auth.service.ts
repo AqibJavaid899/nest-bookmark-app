3588d1c1b4862cd8f229fe31012800cca87d8a7c
AqibJavaid899 <aqibjaved0910@gmail.com>
2023-05-23 16:54:17 +0500

    feature: implement the authentication service logic with all the basic edge cases and also add relationships and constraints in the prisma schema

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import * as argon from "argon2";
 
 import { PrismaService } from "../prisma/prisma.service";
 import { AuthDto } from "./dto";
@@ -7,11 +8,56 @@ import { AuthDto } from "./dto";
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup(dto: AuthDto) {
-    return { message: "I am signing up." };
+  async signup(dto: AuthDto) {
+    // create the hash password
+    const hashedPassword = await argon.hash(dto.password);
+
+    try {
+      // save the user in the db
+      const savedUser = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hashedPassword,
+        },
+      });
+
+      // deleting the hashedPassword from the saved user object
+      const { password, ...user } = savedUser;
+
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error.code === "P2002") {
+        throw new ForbiddenException(
+          "Credentials already taken by another user."
+        );
+      }
+
+      throw error;
+    }
   }
 
-  signin(dto: AuthDto) {
-    return { message: "I am logging in." };
+  async signin(dto: AuthDto) {
+    // Find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    // Throw error if the user does not exist
+    if (!user)
+      throw new ForbiddenException("Provided credentials are incorrect.");
+
+    // Compare the password
+    const isMatched = await argon.verify(user.password, dto.password);
+
+    // Throw error if password does not matched
+    if (!isMatched)
+      throw new ForbiddenException("Provided credentials are incorrect.");
+
+    // Return user without hash password
+    const { password, ...userObject } = user;
+    return userObject;
   }
 }
